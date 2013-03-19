36887996fcc6dc57ffbfe03be4b5af44a72fd269
Glenn Marcus <glenn@cliqconsulting.com>
2013-03-19 18:03:05 -0400

    Finish user edit, update, index and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 require 'spec_helper'
@@ -31,6 +32,21 @@ describe User do
   
   it { should be_valid }
   
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+    
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
